433fc76e9bdfd2b439afc2b506c8131e44fed7eb
MICHA3L-CT <721242@sandwellcollege.ac.uk>
2026-04-23 23:28:58 +0100

    Docx

--- GreenField Local Hub web/E-commerce/obj/Debug/net9.0/E-commerce.AssemblyInfo.cs
+++ GreenField Local Hub web/E-commerce/obj/Debug/net9.0/E-commerce.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("E-commerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71653ba2aaad99689afbb7c13f33330b47ceb800")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15ceceb31b7665a755ade93f40bbea0b6f1722f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("E-commerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("E-commerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
